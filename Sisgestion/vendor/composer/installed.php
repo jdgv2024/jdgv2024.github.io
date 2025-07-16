76cb162539e16e134f98aefded299889dc78225a
jdgv2024 <jean.gutierrez@uac.edu.co>
2025-07-15 19:56:30 -0500

    Update installed.php

--- Sisgestion/vendor/composer/installed.php
+++ Sisgestion/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3e147155c0642e48afd14af08c1540a249d05db3',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3e147155c0642e48afd14af08c1540a249d05db3',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
